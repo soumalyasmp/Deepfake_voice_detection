a7452bbd9617c06f4ef8404579ae971035c3e2be
soumalyasmp <soumalyasmp1845@gmail.com>
2026-07-13 09:35:23 +0530

    deployment update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://deepfake-voice-detection-pi.vercel.app/"
+        "https://deepfake-voice-detection-pi.vercel.app"
     ],
     credentials: true
 }));
